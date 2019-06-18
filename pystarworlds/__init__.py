c9e96503bd7f404bbe819400ec5a15056b99754c
Ben Wilkins <brjw@hotmail.co.uk>
2019-06-18 16:19:13 +0100

    as package

---
+++ pystarworlds/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 18 16:14:41 2019
+
+@author: ben
+"""
+
+from . import Action, Actuator, Agent, Environment, Event, Identification, Perception, Sensor
+
+__all__ = ('Action', 'Actuator', 'Agent', 'Environment', 'Event', 'Identification', 'Perception', 'Sensor')
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 19 13:12:18 2019
+
+@author: ben
+"""
+
+from setuptools import setup
+
+setup(name='pystarworlds',
+      version='0.0.1a',
+      description='',
+      url='',
+      author='Nausheen Saba, Benedict Wilkins',
+      author_email='nausheen.shahid.2017@live.rhul.ac.uk, brjw@hotmail.co.uk',
+      license='MIT',
+      packages=['pystarworlds'],
+      zip_safe=False)
