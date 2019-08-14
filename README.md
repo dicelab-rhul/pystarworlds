66830819d4e030e2c2554a53e73c51a3f59e2fc2
Ben Wilkins <brjw@hotmail.co.uk>
2019-08-14 15:24:39 +0100

    name change, added license

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# pystar
+# pystarworlds
 
 LOCAL INSTALL PROCEEDURE:
 
 1. Clone the repository
-2. Navigate to the pystar directory (the one that contains setup.py) in terminal
+2. Navigate to the pystarworlds directory (the one that contains setup.py) in terminal
 3. pip install -e .
--- setup.py
+++ setup.py
@@ -8,12 +8,16 @@ Created on Tue Mar 19 13:12:18 2019
 
 from setuptools import setup
 
-setup(name='pystar',
+setup(name='pystarworlds',
       version='0.0.1a',
       description='',
-      url='',
-      author='Nausheen Saba, Benedict Wilkins',
-      author_email='nausheen.shahid.2017@live.rhul.ac.uk, brjw@hotmail.co.uk',
-      license='MIT',
-      packages=['pystar'],
-      zip_safe=False)
+      url='https://github.com/dicelab-rhul/pystarworlds',
+      author='Nausheen Saba, Benedict Wilkins, Joel Clarke',
+      author_email='nausheen.shahid.2017@live.rhul.ac.uk, brjw@hotmail.co.uk, joel.clarke273@gmail.com',
+      license='Pending',
+      packages=setuptools.find_packages(),
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv3 License",
+        "Operating System :: OS Independent",
+    ])
