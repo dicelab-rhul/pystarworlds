42b6245fc017a13893c43f8d5033dd62303e8353
Ben Wilkins <brjw@hotmail.co.uk>
2019-08-14 15:43:13 +0100

    changes to setup

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ LOCAL INSTALL PROCEEDURE:
 1. Clone the repository
 2. Navigate to the pystarworlds directory (the one that contains setup.py) in terminal
 3. pip install -e .
+
+
+PIP UPLOAD
+
+python3 setup.py sdist bdist_wheel
+python -m twine upload dist/* 
--- setup.py
+++ setup.py
@@ -6,18 +6,19 @@ Created on Tue Mar 19 13:12:18 2019
 @author: Benedict Wilkins
 """
 
-from setuptools import setup
+import setuptools
 
-setup(name='pystarworlds',
+setuptools.setup(name='pystarworlds',
       version='0.0.1a',
       description='',
       url='https://github.com/dicelab-rhul/pystarworlds',
       author='Nausheen Saba, Benedict Wilkins, Joel Clarke',
       author_email='nausheen.shahid.2017@live.rhul.ac.uk, brjw@hotmail.co.uk, joel.clarke273@gmail.com',
-      license='Pending',
+      license='GNU General Public License v3 (GPLv3)',
       packages=setuptools.find_packages(),
       classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPLv3 License",
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ])
